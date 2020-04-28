13401936f0bdb981c8f96b878a415487c252034c
Alaa Masoud <alaa.masoud@live.com>
2020-04-28 03:03:50 +0300

    Update test output

--- benchmarks/Program.cs
+++ benchmarks/Program.cs
@@ -8,6 +8,23 @@ namespace benchmarks
     {
         static void Main(string[] args)
         {
+            const string reportFile =
+@"``` ini
+
+BenchmarkDotNet=v0.12.1, OS=Windows 10.0.18363.778 (1909/November2018Update/19H2)
+Intel Core i9-9900K CPU 3.60GHz (Coffee Lake), 1 CPU, 16 logical and 8 physical cores
+.NET Core SDK=5.0.100-preview.2.20176.6
+  [Host]     : .NET Core 3.1.3 (CoreCLR 4.700.20.11803, CoreFX 4.700.20.12001), X64 RyuJIT
+  Job-BILSGC : .NET Core 3.1.3 (CoreCLR 4.700.20.11803, CoreFX 4.700.20.12001), X64 RyuJIT
+
+PowerPlanMode=00000000-0000-0000-0000-000000000000  IterationTime=250.0000 ms  MaxIterationCount=20  
+MinIterationCount=15  WarmupCount=1  
+
+```
+|     Method |     Mean |   Error |  StdDev |  Gen 0 |  Gen 1 | Gen 2 | Allocated |
+|----------- |---------:|--------:|--------:|-------:|-------:|------:|----------:|
+| TestMethod | 115.2 μs | 0.51 μs | 0.43 μs | 9.2593 | 0.9259 |     - |  79.12 KB |";
+
             var root = new DirectoryInfo(Path.Join(Path.GetDirectoryName(typeof(Program).Assembly.Location), "..", "..", "..")).FullName;
             var dir = Path.Join(root, ".bdn", "results");
             Directory.CreateDirectory(dir);
@@ -16,7 +33,8 @@ namespace benchmarks
             var lastCommitMsg = Read("git.exe", "log -1 --pretty=%B", noEcho: true);
 
             var sb = new StringBuilder();
-            sb.AppendLine("## Test Report");
+            sb.Append(reportFile);
+            sb.AppendLine();
             sb.AppendLine();
             var filter = args.Length == 2 && args[0] == "--filter" ? args[1] : "";
             sb.AppendLine($"Filter: {filter}");
